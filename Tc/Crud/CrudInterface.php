9836371eeb4399d77d2992397c1d059ae5f9d0c8
Georgiana Gligor <g@lolaent.com>
2014-12-10 18:12:11 +0200

    sketch interface for Crud

---
+++ Tc/Crud/CrudInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tc\Crud;
+
+/**
+ * Describes a data accessor API.
+ */
+interface Crud
+{
+    
+    /**
+     * Create new item
+     */
+    public function create($item = null);
+    
+    /**
+     * Retrieve existing item
+     *
+     * @return object|null
+     */
+    public function read();
+    
+    /**
+     * Changes existing item
+     */
+    public function update();
+    
+    /**
+     * Remove existing item
+     */
+    public function delete();
+    
+    /**
+     * Insert new or update existing item, if one exists
+     */
+    public function upsert($item = null);
+    
+}
