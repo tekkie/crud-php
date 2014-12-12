c8b7074dc01bba91619d16264be2f893e6c6e5f0
Georgiana Gligor <g@lolaent.com>
2014-12-12 11:09:35 +0200

    Improve method signatures to match broader scenarios.

--- Tc/Crud/CrudInterface.php
+++ Tc/Crud/CrudInterface.php
@@ -10,25 +10,36 @@ interface Crud
     
     /**
      * Create new item
+     *
+     * @param object $item
+     * @param array  $criteria
      */
-    public function create($item = null);
+    public function create($item = null, array $criteria = array());
     
     /**
      * Retrieve existing item
      *
+     * @param array $criteria
+     * @param array $fields
+     *
      * @return object|null
      */
-    public function read();
+    public function read(array $criteria = array(), $fields = array());
     
     /**
      * Changes existing item
+     *
+     * @param object $item
+     * @param array  $criteria
      */
-    public function update();
+    public function update($item, array $criteria = array());
     
     /**
      * Remove existing item
+     *
+     * @param object $item
      */
-    public function delete();
+    public function delete($item);
     
     /**
      * Insert new or update existing item, if one exists
